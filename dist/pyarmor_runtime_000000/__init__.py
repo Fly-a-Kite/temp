4db74a84c19f6252b7244bf8d57a44f02d3882ca
fly <fly@flydeMac-mini.local>
2025-02-21 18:49:21 +0800

    one

--- dist/pyarmor_runtime_000000/__init__.py
+++
@@ -1,2 +0,0 @@
-# Pyarmor 9.0.7 (trial), 000000, 2025-02-20T23:56:29.806151
-from .pyarmor_runtime import __pyarmor__
---
+++ pyarmor_runtime_000000/__init__.py
@@ -0,0 +1,2 @@
+# Pyarmor 9.0.7 (trial), 000000, 2025-02-21T18:48:40.386743
+from .pyarmor_runtime import __pyarmor__
